4677826bac9fc9aae314b4a13d760384a621ecb0
Samuel - Work <s-bridge@live.co.uk>
2014-06-05 08:46:24 +0100

    [Open in] - Refined Code

--- User/open_in.py
+++ User/open_in.py
@@ -11,45 +11,15 @@ class file_open(sublime_plugin.EventListener):
 		if file_name.find('.js') > 0:
 			window.set_view_index(view, 1, 0)
 			window.focus_group(1)
-			window.run_command('set_layout', {
-
-				"rows": [0.0, 0.02, 1.0],
-		        "cols": [0.0, 0.02, 1.0],
-		        "cells": [
-		            [1, 0, 2, 1],
-		            [1, 1, 2, 2],
-		            [0, 0, 1, 2]
-		        ]
-			
-			})
+			window.run_command('switch_window_js')
 
 		elif file_name.find('.scss') > 0 or file_name.find('.css') > 0 or file_name.find('.less') > 0:
 			window.set_view_index(view, 0, 0)
 			window.focus_group(0)
-			window.run_command('set_layout', {
-
-				"cols": [0.0, 0.02, 1.0],
-		        "rows": [0.0, 0.95, 1.0],
-		        "cells": [
-		            [1, 0, 2, 1],
-		            [1, 1, 2, 2],
-		            [0, 0, 1, 2]
-		        ]
-
-			})
+			window.run_command('switch_window_css')
 
 		else:
 
 			window.set_view_index(view, 2, 0)
 			window.focus_group(2)
-			window.run_command('set_layout', {
-
-				"cols": [0.0, 0.98, 1.0],
-		        "rows": [0.0, 0.5, 1.0],
-		        "cells": [
-		            [1, 0, 2, 1],
-		            [1, 1, 2, 2],
-		            [0, 0, 1, 2]
-		        ]
-
-			})
+			window.run_command('switch_window_normal')
